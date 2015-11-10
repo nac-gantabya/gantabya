7642a3d82cf3dbcc847b3150fbb150414d6f24ce
Manish Munikar <munikarmanish@gmail.com>
2015-11-10 07:00:43 +0545

    used attribute instead of inner-tag in xml

--- data.xml.php
+++ data.xml.php
@@ -1,36 +1,40 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <GantabyaData>
-    
+
     <?php foreach ($packages as $p): ?>
-    
-    <Package>
-        
-        <PackageId><?php htmlout($p['PackageId']); ?></PackageId>
-        <PackageName><?php htmlout($p['PackageName']); ?></PackageName>
-        <PackagePlace><?php htmlout($p['PackagePlace']); ?></PackagePlace>
-        <PackageDescription><?php htmlout($p['PackageDescription']); ?></PackageDescription>
-        <PackageDuration><?php htmlout($p['PackageDuration']); ?></PackageDuration>
-        <PackagePrice><?php htmlout($p['PackagePrice']); ?></PackagePrice>
-        <PackageDetail><?php htmlout($p['PackageDetail']); ?></PackageDetail>
-        <PackageImage><?php htmlout($p['PackageImage']); ?></PackageImage>
-        <CompanyId><?php htmlout($p['CompanyId']); ?></CompanyId>
-        <CompanyName><?php htmlout($p['CompanyName']); ?></CompanyName>
-        <CompanyWebsite><?php htmlout($p['CompanyWebsite']); ?></CompanyWebsite>
-        
-        <!-- get list of types associated with this package -->
-        <PackageTypes>
-        <?php
-        $q = 'SELECT PackageType.TypeId AS TypeId, Types.Name AS TypeName'.
-                ' FROM Types INNER JOIN PackageType ON Types.Id=PackageType.TypeId'.
-                ' WHERE PackageType.PackageId='.$p['PackageId'];
-        
-        $result = $DB->query($q);
-        while ($t = $result->fetch()): ?>
-            <Type Id="<?php htmlout($t['TypeId']); ?>" Name="<?php htmlout($t['TypeName']); ?>"/>
-        <?php endwhile; ?>
-        </PackageTypes>
-        
-    </Package>
-    
+
+        <!-- beginning of a package -->
+        <Package
+            Id="<?php htmlout($p['PackageId']); ?>"
+            Name="<?php htmlout($p['PackageName']); ?>"
+            Place="<?php htmlout($p['PackagePlace']); ?>"
+            Description="<?php htmlout($p['PackageDescription']); ?>"
+            Duration="<?php htmlout($p['PackageDuration']); ?>"
+            Price="<?php htmlout($p['PackagePrice']); ?>"
+            Detail="<?php htmlout($p['PackageDetail']); ?>"
+            Image="<?php htmlout($p['PackageImage']); ?>">
+
+            <!-- company -->
+            <Company
+                Id="<?php htmlout($p['CompanyId']); ?>"
+                Name="<?php htmlout($p['CompanyName']); ?>"
+                Website="<?php htmlout($p['CompanyWebsite']); ?>"/>
+
+            <!-- associated package types -->
+            <PackageTypes>
+                <?php
+                $q = 'SELECT PackageType.TypeId AS TypeId, Types.Name AS TypeName' .
+                        ' FROM Types INNER JOIN PackageType ON Types.Id=PackageType.TypeId' .
+                        ' WHERE PackageType.PackageId=' . $p['PackageId'];
+
+                $result = $DB->query($q);
+                while ($t = $result->fetch()): ?>
+                    <Type Id="<?php htmlout($t['TypeId']); ?>" Name="<?php htmlout($t['TypeName']); ?>"/>
+                <?php endwhile; ?>
+            </PackageTypes>
+
+        </Package>
+
     <?php endforeach; ?>
-    
+
 </GantabyaData>
